24dc3bae583e3cfadf37661c920b2b876e6dd9cb
nouronallah <nour.onallah1234@gmail.com>
2025-11-27 23:38:32 +0300

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,15 +1,15 @@
-def create_codon_dict(file_path):
+def test_create_codon_dict(file_path):
     codon_dict = {}
-
+    
     file = open(file_path)
     rows = file.readlines()
-
+    
     for row in rows:
         parts = row.strip().split('\t')
         codon = parts[0]
-        amino_acid = parts[1]
+        amino_acid = parts[2]   
         codon_dict[codon] = amino_acid
-
+    
     file.close()
     return codon_dict
 
